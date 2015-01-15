672a5eb1e93d3ef4f16e54875fb4d6fc32e31e00
Kevin Whelan <Kevinwhelan2098@gmail.com>
2015-01-15 15:20:43 -0500

    Add new spec test files

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,1 +1,19 @@
 require_relative '../lib/dice'
+
+RSpec.describe 'dice' do
+  let(:sides) {6}
+  subject(:dice) {Dice.new(sides)}
+
+  describe 'initialize' do
+    it 'creates a new instance of a Dice with a number of sides' do
+      expect(dice).to be_a Dice
+    end
+  end
+
+describe '#dice.roll' do
+  it 'returns a random number within the scope of the sides of the dice' do
+  expect(dice.roll).to be_between(1, dice.sides).inclusive
+end
+
+end
+end
